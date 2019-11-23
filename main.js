ecc88c3d2407ce9b92330a00af4437867d412c77
Anatoly Levchik <endight@gmail.com>
2019-11-23 23:23:49 +0300

    node autoreload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const { app, BrowserWindow } = require('electron')
 
+require('electron-reload')(__dirname);
+
 function createWindow () {
   let win = new BrowserWindow({
     width: 800,
--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-	let name = 'world';
+	let name = 'new test';
 </script>
 
 <h1>Hello {name}!</h1>
