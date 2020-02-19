fd847f42e7dc5b63c8f27d00930c12603411986a
Anatoly Levchik <endight@gmail.com>
2020-02-19 19:50:56 +0300

    kek

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
-const { app, BrowserWindow, autoUpdater } = require('electron');
+const { app, BrowserWindow, autoUpdater, dialog } = require('electron');
 const runBackend = require('./run_backend');
 
 const server = 'https://hazel.mirth.now.sh';
 const feed = `${server}/update/${process.platform}/${app.getVersion()}`;
 
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Restart', 'Later'],
+    title: 'Application Update',
+    message: process.platform === 'win32' ? releaseNotes : releaseName,
+    detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+  }
+
+  dialog.showMessageBox(dialogOpts).then((returnValue) => {
+    if (returnValue.response === 0) autoUpdater.quitAndInstall()
+  })
+})
+
+console.log('feed: ', feed)
 autoUpdater.setFeedURL(feed);
+
+autoUpdater.checkForUpdates();
 
 if (process.env.ENV === 'dev') {
   // eslint-disable-next-line global-require
