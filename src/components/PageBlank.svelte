e9bfbcb91828664a12996a53c61660a1dc03974e
Anatoly Levchik <endight@gmail.com>
2020-02-25 23:26:43 +0300

    PageBlank refactor

--- src/components/PageBlank.svelte
+++ src/components/PageBlank.svelte
@@ -1,5 +1,8 @@
 <script>
 import { onMount } from 'svelte';
+import Container from "svelte-atoms/Grids/Container.svelte";
+import Row from "svelte-atoms/Grids/Row.svelte";
+import Cell from "svelte-atoms/Grids/Cell.svelte";
 import { fetchProjectList } from '../store';
 import SideBar from './SideBar.svelte';
 
@@ -7,16 +10,18 @@ onMount(fetchProjectList);
 
 </script>
 
-<style>
-#grid {
-  display: grid;
-  height: 100px;
-  grid-template-columns: repeat(6, 1fr);
-  grid-template-rows: 100px;
-}
-</style>
 
+<Container>
 <div id='grid'>
-  <SideBar />
-  <slot></slot>
+  <Row>
+    <Cell xs={2}>
+      <SideBar />
+    </Cell>
+    <Cell xs={10}>
+      <Container>
+        <slot></slot>
+      </Container>
+    </Cell>
+  </Row>
 </div>
+</Container>
