977ce7930d10a761265d46fdb0dac43385ed907d
Anatoly Levchik <endight@gmail.com>
2020-03-27 21:01:10 +0300

    testing publishing

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 const { app, BrowserWindow } = require('electron');
+const { autoUpdater } = require('electron-updater');
 const runBackend = require('./run_backend');
+
+
+autoUpdater.setFeedURL({
+  provider: 'github',
+  repo: 'markuper',
+  owner: 'mirth',
+  private: true,
+  token: process.env.ENV.GH_TOKEN,
+});
 
 if (process.env.ENV === 'dev') {
   // eslint-disable-next-line global-require
@@ -29,6 +39,12 @@ function createWindow(backend) {
 
 function runApp() {
   const backend = runBackend();
+
+  if (process.env.ENV === 'dev') {
+    autoUpdater.checkForUpdates();
+  } else {
+    autoUpdater.checkForUpdatesAndNotify();
+  }
 
   createWindow(backend);
 }
