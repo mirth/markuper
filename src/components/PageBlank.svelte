f673ce047a015f74ac2c81ed9f4ed15207f748c3
Anatoly Levchik <endight@gmail.com>
2020-04-03 14:47:58 +0300

    new side bar

--- src/components/PageBlank.svelte
+++ src/components/PageBlank.svelte
@@ -1,23 +1,21 @@
 <script>
-import { onMount } from 'svelte';
 import Container from 'svelte-atoms/Grids/Container.svelte';
 import Row from 'svelte-atoms/Grids/Row.svelte';
 import Cell from 'svelte-atoms/Grids/Cell.svelte';
-import api from '../api';
+import SideBar from './SideBar.svelte';
 
-let version;
-onMount(async () => {
-  version = await api.get('/version');
-});
+
 
 </script>
 
 
 <Container>
 <div id='grid'>
-  <span>{version}</span>
   <Row>
-    <Cell xs={12}>
+    <Cell xs={1}>
+      <SideBar />
+    </Cell>
+    <Cell xs={11}>
       <Container>
         <Row>
           <Cell>
---
+++ src/components/SideBar.svelte
@@ -0,0 +1,30 @@
+<script>
+import { onMount } from 'svelte';
+import { push } from 'svelte-spa-router';
+import api from '../api';
+import IconButton from 'svelte-atoms/IconButton.svelte';
+import Spacer from 'svelte-atoms/Spacer.svelte';
+
+let version;
+onMount(async () => {
+  version = await api.get('/version');
+});
+</script>
+
+<div>
+  <span><small>{version}</small></span>
+  <Spacer size={16} />
+  <IconButton
+    icon='monitor'
+    on:click={() => push(`/`)}
+    type='filled' status='primary'
+    style='display: inline;'
+  />
+</div>
+
+
+<style>
+div {
+  text-align: center;
+}
+</style>
